e7ea82e5cd320bf5ed2e3cc742a997b3d7d81095
Robert <robert.pakkanen@helsinki.fi>
2019-11-14 18:37:07 +0200

    exercise 8.

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,11 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Random;
 
 public class Tester {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "F:\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",
+                "F:\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
@@ -21,14 +23,53 @@ public class Tester {
         sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("asd");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("asd12345");
         element = driver.findElement(By.name("login"));
         
         sleep(2);
         element.submit();
-
+        
+        sleep(2);
+        element = driver.findElement(By.id("error"));
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("back to home"));
+        element.click();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        
+        sleep(2);
+        
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("arto"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("asd");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("asd");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+        
+        sleep(2);
+        element = driver.findElement(By.xpath("//*[contains(text(), 'Welcome to Ohtu Application!')]"));
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+        
+        sleep(2);
+        element = driver.findElement(By.linkText("login"));
+        
         sleep(3);
         
         driver.quit();
