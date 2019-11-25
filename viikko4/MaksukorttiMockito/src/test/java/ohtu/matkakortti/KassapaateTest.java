fa4cd1d64f2810958e7fa24cdcde40705a7a3aec
Robert <robert.pakkanen@helsinki.fi>
2019-11-25 13:26:22 +0200

    exercise 2.

--- viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,19 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+
+    @Test
+    public void positiivinenSummaLadataanKortille() {
+        kassa.lataa(kortti, 5);
+
+        verify(kortti, times(1)).lataa(eq(5));
+    }
+
+    @Test
+    public void negatiivistaSummaaEiLadataKortille() {
+        kassa.lataa(kortti, -5);
+
+        verify(kortti, times(0)).lataa(anyInt());
+    }
       
 }
